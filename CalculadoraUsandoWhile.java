c594e6c37c602009eabff1b721f14be9fd5b6b9d
Patricia <p.santamartamedina@gmail.com>
2020-12-16 09:59:55 +0100

    metodo que saca multiplos de 5 entre el 5 y el 95

--- CalculadoraUsandoWhile.java
+++ CalculadoraUsandoWhile.java
@@ -1,33 +1,11 @@
 
-/**
- * Write a description of class CalculadoraUsandoWhile here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class CalculadoraUsandoWhile
-{
-    // instance variables - replace the example below with your own
-    private int x;
+public class CalculadoraUsandoWhile{
 
-    /**
-     * Constructor for objects of class CalculadoraUsandoWhile
-     */
-    public CalculadoraUsandoWhile()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public void multiplosDeCinco(){        
+        int multiplo = 5;
+        while(multiplo < (95 - 5)){            
+            System.out.println(multiplo + 5);
+            multiplo = multiplo + 5;
+        }
     }
 }
