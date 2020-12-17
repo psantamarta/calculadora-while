7cb6ace7f6fd476e911420bf0d493482225c40af
Patricia <p.santamartamedina@gmail.com>
2020-12-17 09:53:37 +0100

    se añade metodo sumaDeValores y sumaValoresEnElIntervalo

--- CalculadoraUsandoWhile.java
+++ CalculadoraUsandoWhile.java
@@ -8,4 +8,43 @@ public class CalculadoraUsandoWhile{
             multiplo = multiplo + 5;
         }
     }
+    
+    public void sumaDeValores(){
+        int numero = 0;
+        int suma = 0;
+        while (numero <= 10){
+            suma = suma + numero;
+            numero ++;
+        }
+        System.out.println(suma);    
+    }
+    
+    public int sumaDeValoresEnElIntervalo(int a, int b){
+        int resultado = 0; 
+        int valorA = a;
+        if (a >= 0 && b >= 0){   
+            if (a == b){
+                resultado = a;
+            }
+            else{                
+                int contador = 0;
+                if (b < a){
+                    a = b;
+                    b = valorA;                    
+
+                }
+                int intervalo = b - a;
+                while (contador <= intervalo){
+                    resultado = resultado + a;
+                    a ++;
+                    contador ++;
+                }
+            }
+        }     
+        else{
+            System.out.println("Introduce un número positivo");
+            resultado = -1;
+        }
+        return resultado;
+    }
 }
